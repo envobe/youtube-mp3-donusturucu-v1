85db01576e99f44b2d6b46f80505441f8a847635
envobe <envobe@hotmail.com>
2016-03-23 11:42:16 +0200

    Create veri-cek.php

---
+++ veri-cek.php
@@ -0,0 +1,5 @@
+/* get metodu ile veriyi import ediyoruz. */
+  <?php $icnd=$_GET["icnd"];   ?>
+
+/* frame olarak video donusturucu */
+  <iframe src="http://embed.yt-mp3.com/watch?v=<?php echo $icnd; ?>" style="width: 320px; height: 100px; border: 0px;"></iframe>
