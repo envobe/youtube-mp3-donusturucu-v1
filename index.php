90489e683b1b1c857c4e84e1c0d5cbcc6cbd7bc8
envobe <envobe@hotmail.com>
2016-03-23 11:39:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<!-- mp3 formatina donusturulmesini istedigimiz video URL'sini yaziyoruz. -->
+
+      <form action="isleme_al.php" method="get"> /* GET metodunu kullanarak veriyi çekiyoruz */
+      		<input type="text" name="icnd" placeholder="video URL" />
+      		<button type="submit" id="ara"><span>Onayla</span></button>
+      </form>
