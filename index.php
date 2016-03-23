a4f0399e7216ccbde5bca51938a6c4957911a22a
envobe <envobe@hotmail.com>
2016-03-23 11:43:12 +0200

    Form döngüleri güncelleştirildi.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <!-- mp3 formatina donusturulmesini istedigimiz video URL'sini yaziyoruz. -->
-
       <form action="veri-cek.php" method="get"> /* GET metodunu kullanarak veriyi çekiyoruz */
-      		<input type="text" name="icnd" placeholder="video URL" />
-      		<button type="submit" id="ara"><span>Onayla</span></button>
+            <input type="text" name="icnd" placeholder="video URL" />
+            <button type="submit" id="ara"><span>Onayla</span></button>
       </form>
